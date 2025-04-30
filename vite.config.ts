3215cd41d03a758d5904909cb43b2d79f7bcdbf3
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-04-30 18:14:13 +0300

    feat: add development mode for bundling

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const isDevMode = mode === "development";
+  return {
+    base: "./",
+    build: isDevMode
+      ? {
+          sourcemap: "inline",
+          minify: false,
+          rollupOptions: { output: { manualChunks: undefined } },
+        }
+      : {},
+    plugins: [react()],
+  };
+});
