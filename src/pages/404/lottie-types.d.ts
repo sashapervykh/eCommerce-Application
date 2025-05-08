0f595ac1fd5be24e069130a5895c496274f1a24d
Grigori Konopelko <grigori.konopelko@gmail.com>
2025-05-08 18:49:34 +0300

    feat: add 404 page

---
+++ src/pages/404/lottie-types.d.ts
@@ -0,0 +1,16 @@
+declare module '*.json' {
+  const value: {
+    v: string;
+    fr: number;
+    ip: number;
+    op: number;
+    w: number;
+    h: number;
+    nm: string;
+    ddd: number;
+    assets: unknown[];
+    layers: unknown[];
+    markers: never[];
+  };
+  export default value;
+}
--- src/pages/404/not-found.tsx
+++ src/pages/404/not-found.tsx
@@ -1,15 +1,20 @@
-import { Link } from 'react-router-dom';
-import { Button } from '@gravity-ui/uikit';
+import { Card } from '@gravity-ui/uikit';
+import styles from './style.module.css';
+import NavigationButton from '../../components/navigation-button/navigation-button';
+import { Routes } from '../../components/navigation-button/type';
+import Lottie from 'lottie-react';
+import animationData from './animation.json';
 
 export default function NotFoundPage() {
   return (
-    <div className="page">
-      <h1>NotFoundPage</h1>
-      <Link to="/">
-        <Button view="action" size="l">
-          To main
-        </Button>
-      </Link>
-    </div>
+    <main className={styles.main}>
+      <Card type="container" view="outlined" style={{ padding: '40px' }}>
+        <div>
+          <h1 className={styles.h1}>Page Not Found</h1>
+          <Lottie animationData={animationData} loop={true} style={{ width: '100%', margin: '0 auto' }} />
+          <NavigationButton route={Routes.main} text={'To main'} />
+        </div>
+      </Card>
+    </main>
   );
 }
