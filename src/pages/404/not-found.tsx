002662a7facc60fd477d4f544ab21e1c799541fa
Grigori Konopelko <grigori.konopelko@gmail.com>
2025-05-08 19:21:27 +0300

    fix: path

--- src/pages/404/not-found.tsx
+++ src/pages/404/not-found.tsx
@@ -4,13 +4,16 @@ import NavigationButton from '../../components/navigation-button/navigation-butt
 import { Routes } from '../../components/navigation-button/type';
 import Lottie from 'lottie-react';
 import animationData from './animation.json';
+import { useLocation } from 'react-router';
 
 export default function NotFoundPage() {
+  const location = useLocation();
   return (
     <main className={styles.main}>
       <Card type="container" view="outlined" style={{ padding: '40px' }}>
         <div>
           <h1 className={styles.h1}>Page Not Found</h1>
+          <p>The path {location.pathname} does not exist</p>
           <Lottie animationData={animationData} loop={true} style={{ width: '100%', margin: '0 auto' }} />
           <NavigationButton route={Routes.main} text={'To main'} />
         </div>
