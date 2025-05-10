f7083703c722aa86b01ec82b825d5a10741947d8
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-05-10 14:37:14 +0300

    feat: change visibilty of the password

--- src/components/form-label/form-label.tsx
+++ src/components/form-label/form-label.tsx
@@ -5,6 +5,7 @@ import InnerInputButton from '../inner-input-button/inner-input-button';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { schema } from '../../utilities/validation-config/validation-rules';
+import { useState } from 'react';
 
 export default function FormLabel({
   type,
@@ -25,19 +26,27 @@ export default function FormLabel({
     resolver: zodResolver(schema),
   });
 
+  const [inputType, setInputType] = useState<InputTypes>(type);
+
   return (
     <label className={styles.label}>
       <Text variant="subheader-2">Please enter your {text}:</Text>
       <TextInput
         placeholder={'Enter ' + text}
         className={styles.input}
-        type={type}
+        type={inputType}
         size="xl"
         {...(zScheme && register(zScheme))}
         errorMessage={zScheme && typeof errors[zScheme]?.message === 'string' ? errors[zScheme].message : ''}
         validationState={zScheme && errors[zScheme] ? 'invalid' : undefined}
       ></TextInput>
-      {InnerButton && <InnerButton />}
+      {InnerButton && (
+        <InnerButton
+          onClickFunction={() => {
+            setInputType((previous) => (previous === 'password' ? 'text' : 'password'));
+          }}
+        />
+      )}
     </label>
   );
 }
