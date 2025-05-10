facc81bf890163d489680d12efe034779d7ca7ae
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-05-10 14:58:38 +0300

    fix: fix login validation

--- src/utilities/validation-config/validation-rules.ts
+++ src/utilities/validation-config/validation-rules.ts
@@ -3,7 +3,7 @@ import { isValidPassword } from './validation-functions/is-valid-password';
 
 const customErrorMap: z.ZodErrorMap = (issue, context) => {
   if (issue.code === z.ZodIssueCode.invalid_string && issue.validation === 'email') {
-    return { message: 'Email must not contain spaces and must be well formatted (i.e. include @ and domain)' };
+    return { message: 'Email must be well formatted (i.e. include @ and domain and not contain whitespaces)' };
   }
 
   return { message: context.defaultError };
@@ -12,6 +12,6 @@ const customErrorMap: z.ZodErrorMap = (issue, context) => {
 z.setErrorMap(customErrorMap);
 
 export const schema = z.object({
-  email: z.string().superRefine(isValidPassword),
+  email: z.string().email(),
   password: z.string().superRefine(isValidPassword),
 });
