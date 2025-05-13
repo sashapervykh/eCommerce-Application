9250746e950017ac70e98112f5a07ed6a5186049
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-05-13 13:41:59 +0300

    refactor: delete revalidate mode and SignIbutton component

--- src/components/sign-in-button/type.ts
+++
@@ -1,3 +0,0 @@
-export interface SignInButtonProperties {
-  text: string;
-}
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,10 +1,9 @@
-import { Card, Text } from '@gravity-ui/uikit';
+import { Card, Text, Button } from '@gravity-ui/uikit';
 import styles from './style.module.css';
 import NavigationButton from '../../components/navigation-button/navigation-button';
 import { Routes } from '../../components/navigation-button/type';
 import FormLabel from '../../components/form-label/form-label';
 import InnerInputButton from '../../components/inner-input-button/inner-input-button';
-import SignInButton from '../../components/sign-in-button/sign-in-button';
 import { FormProvider, useForm } from 'react-hook-form';
 import { schema } from '../../utilities/validation-config/validation-rules';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -16,7 +15,6 @@ const onSubmit = (data: { email: string; password: string }) => {
 export default function LoginPage() {
   const methods = useForm({
     mode: 'onChange',
-    reValidateMode: 'onChange',
     resolver: zodResolver(schema),
   });
   const { handleSubmit } = methods;
@@ -33,7 +31,9 @@ export default function LoginPage() {
             <h1 className={styles.h1}>Log Into Your Account</h1>
             <FormLabel zScheme="email" type="text" text="e-mail" />
             <FormLabel zScheme="password" type="password" text="password" InnerButton={InnerInputButton} />
-            <SignInButton text={'Sign in'} />
+            <Button type="submit" view="action" size="xl" width="max">
+              Sign in
+            </Button>
             <div className={styles['signup-wrapper']}>
               <Text variant="subheader-1">Don't have an account? Sign up here:</Text>
               <NavigationButton route={Routes.registration} text={'Sign up'} />
