7622796e8ce40832a6624630b15e5ac9b86d480e
Evgeniy <e.valerevich@gmail.com>
2025-05-19 13:57:34 +0300

    feat: add favicon

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import { App } from './app';
 import ReactDOM from 'react-dom/client';
 
+const favicon = document.createElement('link');
+favicon.rel = 'icon';
+favicon.href = 'assets/favicon.ico';
+document.head.appendChild(favicon);
+
 const container = document.createElement('div');
 document.body.append(container);
 ReactDOM.createRoot(container).render(<App />);
