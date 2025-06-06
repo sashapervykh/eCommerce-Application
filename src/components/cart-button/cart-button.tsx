41b35a1dff5b71a7b5b96060a15f15949cc07991
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-06-06 13:22:30 +0300

    feat: show products amount in button

--- src/components/cart-button/cart-button.tsx
+++ src/components/cart-button/cart-button.tsx
@@ -1,12 +1,21 @@
-import { Button } from '@gravity-ui/uikit';
+import { Button, Label } from '@gravity-ui/uikit';
 import { useNavigate } from 'react-router-dom';
+import styles from './styles.module.css';
+import { useCart } from '../hooks/useCart';
 
 export function CartButton({ buttonSize, buttonWidth }: { buttonSize: 'm' | 'l'; buttonWidth: 'max' | undefined }) {
   const navigate = useNavigate();
+  const { productsInCartAmount, updateProductsInCartAmount } = useCart();
+  updateProductsInCartAmount();
 
   return (
     <Button view="action" onClick={() => navigate('/cart')} size={buttonSize} width={buttonWidth}>
-      Cart
+      <div className={styles['button-content']}>
+        <div>Cart</div>
+        <Label className={styles['products-number']} theme="unknown">
+          {productsInCartAmount}
+        </Label>
+      </div>
     </Button>
   );
 }
