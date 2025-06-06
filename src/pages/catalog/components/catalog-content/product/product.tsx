0d40090a0374c11583d60b635c39df4fe6c2b3cf
Evgeniy <e.valerevich@gmail.com>
2025-06-06 16:14:15 +0300

    style: aligned the button to the bottom edge

--- src/pages/catalog/components/catalog-content/product/product.tsx
+++ src/pages/catalog/components/catalog-content/product/product.tsx
@@ -14,27 +14,31 @@ export function ProductCard({ productInfo }: { productInfo: ProductInfo }) {
       className={styles.wrapper}
       onClick={() => navigate(`/products/${productInfo.key}`)}
     >
-      <div className={styles['image-wrapper']}>
-        {productInfo.images?.map((image, index) => {
-          return (
-            <div key={index}>
-              <img className={styles.image} src={image.url} alt={image.label} />
-            </div>
-          );
-        })}
-      </div>
+      <div className={styles['content-wrapper']}>
+        <div className={styles['image-wrapper']}>
+          {productInfo.images?.map((image, index) => {
+            return (
+              <div key={index}>
+                <img className={styles.image} src={image.url} alt={image.label} />
+              </div>
+            );
+          })}
+        </div>
 
-      <div className={styles['text-wrapper']}>
-        <Text variant="body-2" className={styles.text}>
-          <b>Name:</b> {productInfo.name}
-        </Text>
-        <Text variant="body-2" className={styles.text}>
-          <b>Price:</b> ${productInfo.price}{' '}
-          {productInfo.fullPrice ? <span className={styles['full-price']}>${productInfo.fullPrice}</span> : ''}
-        </Text>
-        <Text variant="body-2" className={(styles.text, styles.description)} ellipsis={true} ellipsisLines={5}>
-          <b>Description:</b> {productInfo.description}
-        </Text>
+        <div className={styles['text-wrapper']}>
+          <Text variant="body-2" className={styles.text}>
+            <b>Name:</b> {productInfo.name}
+          </Text>
+          <Text variant="body-2" className={styles.text}>
+            <b>Price:</b> ${productInfo.price}{' '}
+            {productInfo.fullPrice ? <span className={styles['full-price']}>${productInfo.fullPrice}</span> : ''}
+          </Text>
+          <Text variant="body-2" className={(styles.text, styles.description)} ellipsis={true} ellipsisLines={5}>
+            <b>Description:</b> {productInfo.description}
+          </Text>
+        </div>
+      </div>
+      <div className={styles['actions-wrapper']}>
         <AddToCartButton product={productInfo} />
       </div>
     </Card>
